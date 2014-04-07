f65347c16c252dae93fc97263cacd42a03f21e76
Mihai Mogosanu <mike@sapiensworks.com>
2014-04-07 13:18:23 -0400

    added tests for WebApi RouteFromParametersConvention

--- src/WebApiPowerTools/Routing/Conventions/RouteFromParametersConvention.cs
+++ src/WebApiPowerTools/Routing/Conventions/RouteFromParametersConvention.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using System.Web.Http;
@@ -5,58 +6,29 @@ using System.Web.Http.Routing;
 
 namespace WebApiPowerTools.Routing.Conventions
 {
-    public class RouteFromParametersConvention : RoutingConventionsModule
+    public class RouteFromParametersConvention : IBuildRoutes
     {
-    
-        public override void Configure(IConfigureRoutingConventions routing)
-        {
-            routing.Always().Build(builderHelper =>
-            {
-
-                var route = builderHelper.CreateRoute(GenerateRouteTemplate(builderHelper));
-                
-                builderHelper.ActionCall.SetParamsDefaults(route.Defaults);
-                foreach (var arg in builderHelper.ActionCall.GetArgumentNames())
-                {
-                    IHttpRouteConstraint constraint = GetConstraint(builderHelper, arg);
-                    if (constraint != null)
-                    {
-                        route.Constraints[arg] = constraint;
-                    }
-                }
-
-                MethodInfo action = builderHelper.ActionCall.Method;
-                if (action.Name.StartsWith("Get") || action.Name.StartsWith("Find") ||
-                    action.GetCustomAttribute<HttpGetAttribute>() != null)
-                {
-                    route.ConstrainToGet();
-                }
-
-                if (action.Name.StartsWith("Post") || action.GetCustomAttribute<HttpPostAttribute>() != null)
-                {
-                    route.ConstrainToPost();
-                }
-                return new[] {route};
-            });
-        }
-
         private static IHttpRouteConstraint GetConstraint(RouteBuilderInfo builderHelper,string arg)
         {
-            ParameterInfo parameter = null;
             var argument = builderHelper.ActionCall.GetActionArgument(arg);
             if (argument!=null)
             {
-                return builderHelper.GetConstraint(parameter.ParameterType);
+                return builderHelper.GetConstraint(argument.ParameterType);
             }
             return null;
         }
 
-        static string GenerateRouteTemplate(RouteBuilderInfo builder)
+        protected virtual string FormatActionName(string name)
+        {
+            return name.ToLower();
+        }
+
+        string GenerateRouteTemplate(RouteBuilderInfo builder)
         {
             var sb=new StringBuilder();
             sb.Append(builder.ActionCall.Controller.ControllerNameWithoutSuffix().ToLower())
                 .Append("/")
-                .Append(builder.ActionCall.Method.Name.ToLower())
+                .Append(FormatActionName(builder.ActionCall.Method.Name))
                 ;
 
             foreach (var arg in builder.ActionCall.GetArgumentNames())
@@ -65,6 +37,39 @@ namespace WebApiPowerTools.Routing.Conventions
                 sb.Append("{" + arg + "}");
             }
             return sb.ToString();
+        }
+
+        public virtual bool Match(ActionCall action)
+        {
+            return true;
+        }
+
+        public IEnumerable<IHttpRoute> Build(RouteBuilderInfo builderHelper)
+        {
+            var route = builderHelper.CreateRoute(GenerateRouteTemplate(builderHelper));
+
+            builderHelper.ActionCall.SetParamsDefaults(route.Defaults);
+            foreach (var arg in builderHelper.ActionCall.GetArgumentNames())
+            {
+                IHttpRouteConstraint constraint = GetConstraint(builderHelper, arg);
+                if (constraint != null)
+                {
+                    route.Constraints[arg] = constraint;
+                }
+            }
+
+            MethodInfo action = builderHelper.ActionCall.Method;
+            if (action.Name.StartsWith("Get") || action.Name.StartsWith("Find") ||
+                action.GetCustomAttribute<HttpGetAttribute>() != null)
+            {
+                route.ConstrainToGet();
+            }
+
+            if (action.Name.StartsWith("Post") || action.GetCustomAttribute<HttpPostAttribute>() != null)
+            {
+                route.ConstrainToPost();
+            }
+            return new[] { route };
         }
     }
 }
---
+++ src/WebApiTests/RouteFromParametersTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using System.Web.Http.Routing.Constraints;
+using FluentAssertions;
+using WebApiPowerTools.Routing;
+using WebApiPowerTools.Routing.Conventions;
+using Xunit;
+
+namespace WebApiTests
+{
+    public class MyController : ApiController
+    {
+        public IEnumerable<string> GetSomething(int id,ApiController user,string data="hi")
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+
+    public class RouteFromParametersTests
+    {
+        private RouteFromParametersConvention _sut;
+        private IHttpRoute _route;
+
+        public RouteFromParametersTests()
+        {
+            var conventions = new RoutingConventions();
+            _sut = new RouteFromParametersConvention();
+            var routes = _sut.Build(new RouteBuilderInfo(new ActionCall(typeof(MyController).GetMethod("GetSomething")), conventions));
+            _route = routes.First();
+        }
+
+        [Fact]
+        public void route_template_is_generated_properly()
+        {
+            _route.RouteTemplate.Should().Be("my/getsomething/{id}/{data}");
+        }
+
+        [Fact]
+        public void int_constraint_is_applied()
+        {
+            _route.Constraints["id"].Should().BeOfType<IntRouteConstraint>();
+        }
+
+        [Fact]
+        public void one_default_has_value()
+        {
+            _route.Defaults.Count.Should().Be(3);//controller + action = 2
+            _route.Defaults["data"].Should().Be("hi");
+        }
+
+        [Fact]
+        public void user_defined_args_are_ignored()
+        {
+            _route.Defaults.ContainsKey("user").Should().BeFalse();
+        }
+
+    }
+}
