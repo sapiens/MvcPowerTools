c2e1595c36a1e8dd4a7f9eaeb2dac5f92bdfd27e
Mihai Mogosanu <mike@sapiensworks.com>
2014-04-09 14:31:36 -0400

    updated sample nugets

--- src/HtmlConventionsSample/App_Start/RouteConfig.cs
+++ src/HtmlConventionsSample/App_Start/RouteConfig.cs
@@ -6,9 +6,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
-using HtmlConventionsSample.Controllers;
-using MvcPowerTools.Routing;
-using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
+
 
 namespace HtmlConventionsSample
 {
@@ -19,20 +17,8 @@ namespace HtmlConventionsSample
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             
             
-            RoutingConventions.Configure(r => r
-                .RegisterController<HomeController>()
-                .RegisterController<QueryController>()
-                .HomeIs<HomeController>(h => h.Index())
-                .DefaultBuilder(a =>
-                {
-                    var url = a.ActionCall.Controller.ControllerNameWithoutSuffix()+"/"+a.ActionCall.Method.Name;
-                    var route = a.CreateRoute(url);
-                    if (a.ActionCall.Method.HasCustomAttribute<HttpPostAttribute>())
-                    {
-                        route.Constraints["method"] = new HttpMethodConstraint("POST");
-                    }
-                    return new[] {route};
-                }));
+           
+               
         }
     }
 }
